d375ef5202f52713ceddf2b46bf7dc8a473f29fd
Balaji Nayak <balajinayak941@gmail.com>
2026-09-15 12:31:47 +0530

    fix: disable cms plugin for production build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import cmsPlugin from './cms-server.js'
 
 export default defineConfig({
-  plugins: [react(), cmsPlugin()],
+  plugins: process.env.NODE_ENV === 'production' ? [react()] : [react(), cmsPlugin()],
   appType: 'spa',
 })
